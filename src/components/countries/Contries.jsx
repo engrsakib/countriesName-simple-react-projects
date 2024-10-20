4e0f64ddfbe8b05a179ebd28d835075358ff05d9
engrsakib <snazmussakib01@gmail.com>
2024-10-20 19:31:51 +0600

    addded visited countries

--- src/components/countries/Contries.jsx
+++ src/components/countries/Contries.jsx
@@ -4,18 +4,33 @@ import Deshh from "./Deshh";
 
 const Contries = () => {
   const [country, setCountry] = useState([]);
+  const[vistCont, setvistCont] = useState([]);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
       .then((data) => setCountry(data));
   }, []);
+
+  const handlevistCont = (country) =>{
+    console.log("add this list");
+    const newVisCont = [...vistCont, country];
+    setvistCont(newVisCont);
+  }
   return (
     <div>
       <h3 className="text-3xl">Countries : {country.length}</h3>
+      <div>
+        <h5 className="text-2xl text-red-600">Visitded Countries {vistCont.length}</h5>
+      </div>
+
       <div className="grid grid-cols-1 md:grid-cols-3">
         {country.map((desh) => (
-          <Deshh key={desh.cca3} desh={desh}></Deshh>
+          <Deshh
+            key={desh.cca3}
+            handlevistCont={handlevistCont}
+            desh={desh}
+          ></Deshh>
         ))}
       </div>
     </div>
--- src/components/countries/Deshh.jsx
+++ src/components/countries/Deshh.jsx
@@ -1,25 +1,33 @@
 import { useState } from "react";
 
-const Deshh = ({desh}) => {
-    const{name, flags, area, population, cca3} = desh;
+const Deshh = ({ desh, handlevistCont }) => {
+  const { name, flags, area, population, cca3 } = desh;
 
-    const[vis, setvis] = useState(false);
-    const handelvis = () =>{
-        setvis(!vis);
-    }
-    return (
-      <div className="country">
-        <h2>Name: {name.common}</h2>
-        <img src={flags.png} alt="" />
-        <p>population: {population}</p>
-        <p>area: {area}</p>
-        <p>country code: {cca3}</p>
-        <button onClick={handelvis} className="btn btn-secondary">{vis?"Visited":"On Going"}</button> <br />
-        {
-            vis ? "I have allrady visited" : "I will visit as soon as possible"
-        }
-      </div>
-    );
+  const [vis, setvis] = useState(false);
+  const handelvis = () => {
+    setvis(!vis);
+  };
+//   console.log(handlevistCont);
+  return (
+    <div className="country">
+      <h2>Name: {name.common}</h2>
+      <img src={flags.png} alt="" />
+      <p>population: {population}</p>
+      <p>area: {area}</p>
+      <p>country code: {cca3}</p>
+      <button onClick={() => handlevistCont(desh)} className="btn btn-error m-3">
+        Mark as visited
+      </button>
+      <button
+        onClick={handelvis}
+        className={`btn ${vis ? "pb" : "btn-secondary"}`}
+      >
+        {vis ? "Visited" : "On Going"}
+      </button>{" "}
+      <br />
+      {vis ? "I have allrady visited" : "I will visit as soon as possible"}
+    </div>
+  );
 };
 
 export default Deshh;
