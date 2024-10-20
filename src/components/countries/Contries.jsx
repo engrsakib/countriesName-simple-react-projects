5fd93a48bf32a186f36f985fda182bfede651c83
engrsakib <snazmussakib01@gmail.com>
2024-10-20 15:38:59 +0600

    data county arr all added

--- src/components/countries/Contries.jsx
+++ src/components/countries/Contries.jsx
@@ -3,21 +3,23 @@ import { useState } from "react";
 import Deshh from "./Deshh";
 
 const Contries = () => {
-    const [country, setCountry] = useState([]);
+  const [country, setCountry] = useState([]);
 
-    useEffect(()=>{
-        fetch("https://restcountries.com/v3.1/all")
-          .then((res) => res.json())
-          .then((data) => setCountry(data));
-    },[])
-    return (
-        <div>
-            <h3 className="text-3xl">Countries : {country.length}</h3>   
-            {
-                country.map(desh => <Deshh key={desh.cca3} desh={desh}></Deshh>)
-            }         
-        </div>
-    );
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountry(data));
+  }, []);
+  return (
+    <div>
+      <h3 className="text-3xl">Countries : {country.length}</h3>
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        {country.map((desh) => (
+          <Deshh key={desh.cca3} desh={desh}></Deshh>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Contries;
--- src/components/countries/Deshh.jsx
+++ src/components/countries/Deshh.jsx
@@ -1,9 +1,23 @@
-const Deshh = ({desh}) => {
-    const{name} = desh;
+import { useState } from "react";
 
+const Deshh = ({desh}) => {
+    const{name, flags, area, population, cca3} = desh;
+
+    const[vis, setvis] = useState(false);
+    const handelvis = () =>{
+        setvis(!vis);
+    }
     return (
       <div className="country">
         <h2>Name: {name.common}</h2>
+        <img src={flags.png} alt="" />
+        <p>population: {population}</p>
+        <p>area: {area}</p>
+        <p>country code: {cca3}</p>
+        <button onClick={handelvis} className="btn btn-secondary">{vis?"Visited":"On Going"}</button> <br />
+        {
+            vis ? "I have allrady visited" : "I will visit as soon as possible"
+        }
       </div>
     );
 };
